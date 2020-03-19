fd660a70aab3495716d6c1b713bba50a6d8fa5f6
Nacho Caiafa <47578311+NachoKai@users.noreply.github.com>
2020-03-18 23:08:06 -0300

    Puede ser que esto vaya así para que funcione?

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ class Telegram {
 
         const telegramMsg = encodeURIComponent(message);
 
-        const url = `https://api.telegram.org/${botId}/sendMessage?chat_id=${chatId}&text=${telegramMsg}`;
+        const url = `https://api.telegram.org/bot${botId}/sendMessage?chat_id=${chatId}&text=${telegramMsg}`;
         axios.get(url);
     }
 }
